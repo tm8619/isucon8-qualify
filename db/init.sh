fcf4578b8dd09837bef076b730322764d0f154b3
tm8619 <matsuzaki_toshiya8619@yahoo.co.jp>
2021-07-18 18:56:06 +0900

    dbのinit修正

--- db/init.sh
+++ db/init.sh
@@ -18,3 +18,4 @@ fi
 mysql -uisucon torb -e 'ALTER TABLE reservations DROP KEY event_id_and_sheet_id_idx'
 gzip -dc "$DB_DIR/isucon8q-initial-dataset.sql.gz" | mysql -uisucon torb
 mysql -uisucon torb -e 'ALTER TABLE reservations ADD KEY event_id_and_sheet_id_idx (event_id, sheet_id)'
+mysql -uisucon torb -e 'INSERT INTO cancelled_reservations SELECT * FROM reservations where canceled_at IS NOT NULL;SET SQL_SAFE_UPDATES = 0;DELETE FROM `reservations` where `canceled_at` IS NOT NULL;ALTER TABLE `reservations` DROP COLUMN `canceled_at`;'
