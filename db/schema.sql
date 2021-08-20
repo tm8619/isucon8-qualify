3de08c1e46e61fd67c9092b1bba00523d800ccb6
tm8619 <matsuzaki_toshiya8619@yahoo.co.jp>
2021-08-20 22:42:29 +0900

    indexを貼る 6000-7000点くらい

--- db/schema.sql
+++ db/schema.sql
@@ -29,7 +29,10 @@ CREATE TABLE IF NOT EXISTS reservations (
     user_id     INTEGER UNSIGNED NOT NULL,
     reserved_at DATETIME(6)      NOT NULL,
     canceled_at DATETIME(6)      DEFAULT NULL,
-    KEY event_id_and_sheet_id_idx (event_id, sheet_id)
+    KEY `event_id_and_sheet_id_idx` (`event_id`,`sheet_id`),
+    KEY `reserved_at_idx` (`reserved_at`),
+    KEY `canceled_at_idx` (`canceled_at`),
+    KEY `user_id_idx` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS administrators (
